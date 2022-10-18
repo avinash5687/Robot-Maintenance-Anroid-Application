8bb919abdb842ff8fe6477ad861ccc9a2f47fa7d
Avinash <avinashmukeshsingh@outlook.com>
2022-10-18 11:05:02 +0530

    login page

---
+++ app/src/main/java/com/example/bajajjhandpm/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.bajajjhandpm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText login_text,password_text;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login_text = (EditText) findViewById(R.id.login);
+        password_text = (EditText) findViewById(R.id.password);
+
+        btnLogin = (Button) findViewById(R.id.buttonLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String username = login_text.getText().toString();
+                String password = password_text.getText().toString();
+
+                if(username.equals("user") && (password.equals("password"))){
+                    Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent (getApplicationContext(),NewActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
